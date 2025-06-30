2ba6e424536baa75464eaf2315d5109a71b42031
Nico <aroca.twelve@proton.me>
2025-06-30 08:25:15 -0400

    removed refresh

--- hti/src/data/events/cgs2021/Layout.tsx
+++ hti/src/data/events/cgs2021/Layout.tsx
@@ -1,18 +1,20 @@
 // CGS 2021 landingpage
+import { Link } from "react-router-dom";
+import './style.css'
 
 
-const CGS21Layout = ({ data }) => { 
+const CGS21Layout = ({ data } : any) => { 
   return (
     <>
-      <div className="p-6 bg-[#251d33] text-white">
+      <div>
         <h1 className="text-4xl mb-6 font-bold">{data.name}</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {data.games.map((game: any) => (
-            <a key={game.id} href={`/events/${data.eventSlug}/${game.slug}`} className="bg-[#322847] p-4 rounded hover:bg-[#3e2d5e] transition">
+            <Link to={`${game.slug}`} key={game.id} className="border p-4 rounded shadow hover:shadow-lg transition">
               <img src={game.thumbnail} className="mb-2 rounded" />
               <h2 className="text-lg font-semibold">{game.title}</h2>
               <p className="text-sm">{game.brief}</p>
-            </a>
+            </Link>
           ))}
         </div>
       </div>
