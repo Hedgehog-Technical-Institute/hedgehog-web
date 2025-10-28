c1956ead4b05b10bcdf9f1a75e9ee3728feae110
Nico <aroca.twelve@proton.me>
2025-10-27 23:09:56 -0400

    finally,,, index.ts

--- hti/src/data/events/2025/cgs/index.ts
+++ hti/src/data/events/2025/cgs/index.ts
@@ -1,3 +1,6 @@
+// index.ts
+import { Event } from "../../../../lib/models"
+
 import { Sonic2Gearshift } from "./Sonic2Gearshift";
 import { SonicErazor } from "./SonicErazor";
 import { SuperMarioBuysBread } from "./SuperMarioBuysBreadLmao";
@@ -10,3 +13,32 @@ import { AlexKidd } from "./AlexKidd";
 import { ProjectForestation } from "./ProjectForestation";
 import { WariosConquest } from "./WariosConquest";
 
+export const CGS2025 = new Event(
+  "cgs2025",
+  "Chao Games Showcase 2025",
+  "TAGLINE GOES HERE",
+  2025,
+  [
+    "DESCRIPTION GOES HERE"
+  ],
+  [
+    "MEDIAURL"    
+  ],
+  "DIRECTURL",
+  [
+    "TREEHOUSELIVESTREAMURL"
+  ],
+  [
+    Sonic2Gearshift,
+    SonicErazor,
+    SuperMarioBuysBread,
+    SATAMCh5,
+    SonicJ,
+    MegaManStarBurst,
+    FreedomPlanetPlus,
+    StarFoxGameWatch,
+    AlexKidd,
+    ProjectForestation,
+    WariosConquest
+  ]
+)
