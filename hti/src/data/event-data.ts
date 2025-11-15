715d5db4882c435e76c2c9821c5eda6d36339b7c
Nico <aroca.twelve@proton.me>
2025-11-15 11:09:16 -0500

    added arc to list

--- hti/src/data/event-data.ts
+++ hti/src/data/event-data.ts
@@ -2,6 +2,8 @@
 import type { Event as EventModel } from "../lib/models";
 import CGS25Layout from "./events/2025/cgs/Layout"; // bare layout below
 import { CGS2025 } from "./events/2025/cgs/index";
+import ARC25Layout from "./events/2025/arc/Layout"; // bare layout below
+import { ARCINDIE2025 } from "./events/2025/arc/index";
 
 export const eventDataMap = {
   cgs2025: {
@@ -9,6 +11,12 @@ export const eventDataMap = {
     name: CGS2025.name,
     layout: CGS25Layout,
     data: CGS2025 as EventModel,
+  },
+  arcindie2025: {
+    id: 7,
+    name: ARCINDIE2025.name,
+    layout: ARC25Layout,
+    data: ARCINDIE2025 as EventModel,
   },
 } satisfies Record<string, {
   id: number
